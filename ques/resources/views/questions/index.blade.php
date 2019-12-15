1669ba46a6253bac25c811e29a6ab8022b933605
LALIT ARYAN <lalitaryan1993@gmail.com>
2019-12-15 20:18:43 +0530

    Implement Authrization using Gates

--- ques/resources/views/questions/index.blade.php
+++ ques/resources/views/questions/index.blade.php
@@ -36,12 +36,18 @@
                                         <a href="{{ $question->url }}">{{ $question->title }}</a>
                                     </h3>
                                     <div class="ml-auto">
+                                        @if(Auth::user()->can('update-question', $question))
                                         <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                        @endif
+                                        @if(Auth::user()->can('delete-question', $question))
+
                                         <form class="d-inline" action="{{ route('questions.destroy', $question->id) }}" method="post">
                                         @method('DELETE')
                                         @csrf
                                             <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure you want to delete this question')">Delete</button>
                                         </form>
+                                        @endif
+
                                     </div>
                                 </div>
 
