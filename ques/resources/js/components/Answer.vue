1140282c83985743641a16d5b135b642677b36eb
LALIT ARYAN <lalitaryan1993@gmail.com>
2020-01-17 14:49:03 +0530

    Beautify the flesh message and confirm dialog

--- ques/resources/js/components/Answer.vue
+++ ques/resources/js/components/Answer.vue
@@ -1,56 +1,99 @@
 <script>
 export default {
-  props: ["answer"],
-  data() {
-    return {
-      editing: false,
-      body: this.answer.body,
-      bodyHtml: this.answer.body_html,
-      id: this.answer.id,
-      questionId: this.answer.question_id,
-      beforeEditCase: null
-    };
-  },
-  methods: {
-    edit() {
-      this.beforeEditCase = this.body;
-      this.editing = true;
+    props: ["answer"],
+    data() {
+        return {
+            editing: false,
+            body: this.answer.body,
+            bodyHtml: this.answer.body_html,
+            id: this.answer.id,
+            questionId: this.answer.question_id,
+            beforeEditCase: null
+        };
     },
-    cancel() {
-      this.body = this.beforeEditCase;
-      this.editing = false;
+    methods: {
+        edit() {
+            this.beforeEditCase = this.body;
+            this.editing = true;
+        },
+        cancel() {
+            this.body = this.beforeEditCase;
+            this.editing = false;
+        },
+        update() {
+            axios
+                .patch(this.endpoint, {
+                    body: this.body
+                })
+                .then(res => {
+                    this.editing = false;
+                    this.bodyHtml = res.data.body_html;
+                    this.$toast.success(res.data.message, "Success", {
+                        timeout: 3000
+                    });
+                })
+                .catch(err => {
+                    this.$toast.error(err.response.data.message, "Error", {
+                        timeout: 3000
+                    });
+                });
+        },
+        destroy() {
+            this.$toast.question("Are you sure about that?", "Confirm", {
+                timeout: 20000,
+                close: false,
+                overlay: true,
+                displayMode: "once",
+                id: "question",
+                zindex: 999,
+                title: "Hey",
+
+                position: "center",
+                buttons: [
+                    [
+                        "<button><b>YES</b></button>",
+                        (instance, toast) => {
+                            axios.delete(this.endpoint).then(res => {
+                                $(this.$el).fadeOut(500, () => {
+                                    this.$toast.success(
+                                        res.data.message,
+                                        "Success",
+                                        {
+                                            timeout: 3000
+                                        }
+                                    );
+                                });
+                            });
+
+                            instance.hide(
+                                { transitionOut: "fadeOut" },
+                                toast,
+                                "button"
+                            );
+                        },
+                        true
+                    ],
+                    [
+                        "<button>NO</button>",
+                        (instance, toast) => {
+                            instance.hide(
+                                { transitionOut: "fadeOut" },
+                                toast,
+                                "button"
+                            );
+                        }
+                    ]
+                ]
+            });
+        }
     },
-    update() {
-      axios
-        .patch(this.endpoint, {
-          body: this.body
-        })
-        .then(res => {
-          this.editing = false;
-          this.bodyHtml = res.data.body_html;
-          alert(res.data.message);
-        })
-        .catch(err => {
-          alert(err.response.data.message);
-        });
-    },
-    destroy() {
-      if (confirm("Are you sure?")) {
-        axios.delete(this.endpoint).then(res => {
-          $(this.$el).fadeOut(500, () => {
-            alert(res.data.message);
-          });
-        });
-      }
+    computed: {
+        isInvalid() {
+            return this.body.length < 10;
+        },
+        endpoint() {
+            return `/questions/${this.questionId}/answers/${this.id}`;
+        }
     }
-  },
-  computed: {
-    isInvalid() {
-      return this.body.length < 10;
-    },
-    endpoint() {
-      return `/questions/${this.questionId}/answers/${this.id}`;
-    }
-  }
 };
 </script>
